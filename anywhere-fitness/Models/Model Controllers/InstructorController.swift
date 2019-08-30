135bafbdf7ad3fd7508fa7151a11960771a32679
Dongwoo Pae <dongwoopae@gmail.com>
2019-08-30 11:48:20 +0900

    commit before pull from master

--- anywhere-fitness/Models/Model Controllers/InstructorController.swift
+++ anywhere-fitness/Models/Model Controllers/InstructorController.swift
@@ -108,6 +108,7 @@ class InstructorController {
                 self.bearer = bearer
                 
                 print(self.bearer!)
+    
                 completion(nil)
                 
             } catch {
